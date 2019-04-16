4d592c80db4bace78a8e4c723d3d401c20d927e1
clockard <chris.lockard@connexta.com>
2019-04-15 21:34:22 -0700

    REP-63 updates from peter?

--- replication-api-impl/src/main/java/org/codice/ditto/replication/api/impl/persistence/ReplicatorHistoryManagerImpl.java
+++ replication-api-impl/src/main/java/org/codice/ditto/replication/api/impl/persistence/ReplicatorHistoryManagerImpl.java
@@ -15,7 +15,6 @@ package org.codice.ditto.replication.api.impl.persistence;
 
 import java.util.stream.Stream;
 import javax.ws.rs.NotFoundException;
-import org.codice.ditto.replication.api.Status;
 import org.codice.ditto.replication.api.data.ReplicationStatus;
 import org.codice.ditto.replication.api.impl.data.ReplicationStatusImpl;
 import org.codice.ditto.replication.api.persistence.ReplicatorHistoryManager;
